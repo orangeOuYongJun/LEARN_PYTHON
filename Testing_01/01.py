b8f4c7ef8b9a074730aaf245d6cc56c7098b8a03
orangeou <orangeou@sohu-inc.com>
2018-02-11 10:46:33 +0800

    【project】【new】2月10日

---
+++ Testing_01/01.py
@@ -0,0 +1,52 @@
+# import numpy
+# print(numpy.__version__)
+
+
+a = 7.
+b = a/3
+
+
+a = range(10)
+# print(a.__len__())
+# b = a[:len(a)-1]
+# # print(len(a))
+# # print(a[-1])
+# # print(b)
+# c = list(range(10))
+# # print(type(c))
+# # print(a)
+# d = a[len(a)-1]
+# print(type(c))
+# b = list(a)
+# # print(type(b))
+# # example_list = [1,2,3,4,5,6,7,12,543,876,12,3,2,5]
+# # print(type(example_list))
+#
+# print(type(a))
+
+# for i in b:
+#     print(i)
+#
+# for i in a:
+#     print(i)
+
+# s = set({'sadf', 'sdfsadf'})
+# print(s)
+#
+# t = ('sadfsf', 'sdfsadf')
+# print(type(t))
+
+# def fib(max):
+#     a, b = 0, 1
+#     # while 1:
+#     yield b
+#     print(a)
+#
+# # c = fib(1).__next__()
+# # d = fib(3).__next__()
+# # print(c)
+# # d = fib(3)
+#
+# for a in fib(2):
+#     print('')       #每执行到yield，返回生成一个迭代器
+#     # print(a)
